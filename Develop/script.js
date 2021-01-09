b0a23b54ce47e9461cacbc3478649b42ba5b64a9
Joe Malone <joe.d.malone@gmail.com>
2021-01-09 17:20:10 -0600

    Logic complete; debugging extra/missing numbers

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,68 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialCharacters = {
+var options = {
   "chars": [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "'\'", "]", "^", "_", "", "{", "|", "}", "~" ],
   "letters": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   "numbers": [0,1,2,3,4,5,6,7,8,9],
   "finalPW": []
-}
+};
+var generatedPassword = '';
 
 // Write password to the #password input
 function generatePassword() {
-  // Receive User input regarding password length
   var pwLength = Number(prompt("How many characters would you like your password to be?"));
+  // If not an Int Value, user Retry;
+  // if (isNaN(pwLength)) {
+  //   alert('Please enter real numbers only; ex.. 1,2,3,4,5');
+  //   return generatePassword();}
+  // else if (!(pwLength >= 8 && pwLength <= 128 )){
+  //   alert("Please enter a number >=0 but <= 128")
+  //   // return generatePassword();}
   var specCharsChoice = confirm("Would you like to use special characters in your password?"); 
-  var lettersChoice = confirm("Would you like to use Letters in your password");
+  var lettersChoice = confirm("Would you like to use letters in your password");
   var numbersChoice = confirm("Would you like to use numbers in your password");
-
-  if (specCharsChoice && lettersChoice && numbersChoice) {
-    var finalPW = specialCharacters.finalPW.concat(specialCharacters.chars).concat(specialCharacters.letters).concat(specialCharacters.numbers);
-    // console.log(finalPW);
-  }
-  // if (lettersChoice){
-  //   specialCharacters.finalPW.push(specialCharacters.letters);
-  // } 
-  // if (numbersChoice) {
-  //   specialCharacters.finalPW.push(specialCharacters.numbersChoice);
-  // }
-
-  // The segmentLength will be calculated depending on how many variants(chars/letter/numbers) the user selects Each segment should be equal
-  var generatedPassword = '';
+    
   function createPW(){
     for (index = 0; index < pwLength;){
       var randomIndex = Math.floor(Math.random() * finalPW.length);
       var generatedChar = finalPW[randomIndex];
       generatedPassword += generatedChar;
       index++;
-      console.log(generatedPassword);
-      // console.log(index, generatedPassword.length);
+      // console.log(index, generatedPassword;
     }
   }
-  // If not an Int Value, user Retry;
-  if (isNaN(pwLength)) {
-    alert('Please enter a REAL number; ex.. 1,2,3,4,5');
-    generatePassword();} 
-  else if (pwLength >= 8 && pwLength <= 128){
-    createPW();
-    return generatedPassword;}
-  else {
-    generatePassword();}
-};
-
-
+  
+  if (specCharsChoice && lettersChoice && numbersChoice) {
+    var finalPW = options.finalPW.concat(options.chars).concat(options.letters).concat(options.numbers);
+  } 
+  else if (!(specCharsChoice) && lettersChoice && numbersChoice ) {
+    var finalPW = options.finalPW.concat(options.letters).concat(options.numbers);
+  } 
+  else if (!(specCharsChoice) && !(lettersChoice) && numbersChoice) {
+    var finalPW = options.finalPW.concat(options.numbers);
+  }
+  else if (!(specCharsChoice) && lettersChoice  && !(numbersChoice)) {
+    var finalPW = options.finalPW.concat(options.letters);
+  }
+  else if (!(specCharsChoice) && lettersChoice  && numbersChoice) {
+    var finalPW = options.finalPW.concat(options.letters).concat(options.numbers);
+  }
+  else if (specCharsChoice && !(lettersChoice)  && !(numbersChoice)) {
+    var finalPW = options.finalPW.concat(options.chars);
+  }
+  else if (specCharsChoice && !(lettersChoice)  && numbersChoice) {
+    var finalPW = options.finalPW.concat(options.chars).concat(options.numbers);
+  }
+  else if (specCharsChoice && lettersChoice  && !(numbersChoice)) {
+    var finalPW = options.finalPW.concat(options.chars).concat(options.letters);
+  }
+  else if (!(specCharsChoice && lettersChoice  && numbersChoice)) {
+    alert('At least one of the Unique Identifiers must be selected!');
+    return generatePassword();
+  }
+  createPW();
+  return generatedPassword;
+}
 
 function writePassword() {
   var password = generatePassword();
@@ -59,11 +72,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
-
-
-// For each choice the user chooses, some list will be appended to include the options; if chars is selected; chars list + empty list
-  // Password must be between 8 and 128 Characters long(initially)
-// generate 1 set of alphabetic arrays/dict object
-// generate 1 set of special Characters/ dict object
-// Total length of array1 & array2 should equal the password length
-  // This should be done with a for (loop... sum of pw.length increases, generating random values until i = userInput
